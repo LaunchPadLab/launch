0fb9749de8c2be7d22806b8e7bdd67a8ed9e2ff9
Ryan Francis <ryan.p.francis@gmail.com>
2015-07-13 10:27:38 -0500

    updated initializer

--- lib/launch/engine.rb
+++ lib/launch/engine.rb
@@ -2,10 +2,11 @@ module Launch
   class Engine < ::Rails::Engine
     isolate_namespace Launch
 
-    initializer "launch.assets.precompile" do |app|
-      app.config.assets.precompile += %w(
-        launch/icons/*
-      )
+    initializer 'launch.assets.precompile' do |app|
+      %w(stylesheets javascripts images).each do |sub|
+        app.config.assets.paths << root.join('app', 'assets', sub).to_s
+      end
+      app.config.assets.precompile << %r(\.(?:png|svg|gif)$)
     end
   end
 end

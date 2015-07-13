df9c0e02f0f2386700c8d678d73d9622d67a2781
Ryan Francis <ryan.p.francis@gmail.com>
2015-07-13 10:15:01 -0500

    precompile icons

--- lib/launch/engine.rb
+++ lib/launch/engine.rb
@@ -1,5 +1,11 @@
 module Launch
   class Engine < ::Rails::Engine
     isolate_namespace Launch
+
+    initializer "launch.assets.precompile" do |app|
+      app.config.assets.precompile += %w(
+        launch/icons/*
+      )
+    end
   end
 end

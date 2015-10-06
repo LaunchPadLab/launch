f790856fbb429c2613b0a8f1ec644502a548a64c
Ryan Francis <ryan.p.francis@gmail.com>
2015-10-05 21:06:27 -0500

    updated the way js components pull in

--- app/assets/javascripts/launch/_components.js
+++ app/assets/javascripts/launch/_components.js
@@ -1,6 +1,6 @@
-// require components/_accordion
-// require components/_alerts
-// require components/_datepicker
-// require components/_modal
-// require components/_tabs
-// require components/_topbar
+//= require launch/components/_accordion
+//= require launch/components/_alerts
+//= require launch/components/_datepicker
+//= require launch/components/_modal
+//= require launch/components/_tabs
+//= require launch/components/_topbar
